a227a6689a6d5d5b53b20fd85ce12d1d24ce912f
skitsanos <info@skitsanos.com>
2022-09-23 10:22:41 +0300

    chore: support for the splashscreen

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,1 +1,18 @@
-export default () => <div>app is running</div>
+import {invoke} from '@tauri-apps/api/tauri';
+import {useEffect} from 'react';
+import {Outlet} from 'umi';
+
+export default () =>
+{
+    useEffect(() =>
+    {
+        invoke('close_splashscreen').then(() =>
+        {
+            console.log('HurlEditor is ready');
+        });
+    }, []);
+
+    return <div>app is running
+        <Outlet/>
+    </div>;
+};
